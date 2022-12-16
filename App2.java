74accce716f93f002efa299afb4afe41969d0391
ultima57 <artyomzar83@gmail.com>
2022-12-16 11:43:46 +0200

    somebody ones told me, that world is gone roll me

---
+++ App2.java
@@ -0,0 +1,10 @@
+package room1;
+
+public class App2 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 57; i++) {
+            System.out.println("i**2=" + i * i);
+        }
+    }
+}
