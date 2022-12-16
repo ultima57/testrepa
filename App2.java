505f1b72ba348283bf3eb429911085b3d92daf57
ultima57 <artyomzar83@gmail.com>
2022-12-16 12:59:52 +0200

    added class wodoxlebus

--- App2.java
+++ App2.java
@@ -6,6 +6,37 @@ public class App2 {
         for (int i = 0; i < 57; i++) {
             System.out.println("i**2=" + i * i);
             System.out.println("abaaba");
+
         }
     }
+}
+
+class Vodoxlebus {
+    private String woda;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((woda == null) ? 0 : woda.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Vodoxlebus other = (Vodoxlebus) obj;
+        if (woda == null) {
+            if (other.woda != null)
+                return false;
+        } else if (!woda.equals(other.woda))
+            return false;
+        return true;
+    }
+
 }
