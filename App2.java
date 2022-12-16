5e18c3053432fac3cfd9fa10af5d86b8ccc699f7
ultima57 <artyomzar83@gmail.com>
2022-12-16 12:53:18 +0200

    tututu

--- App2.java
+++ App2.java
@@ -5,6 +5,7 @@ public class App2 {
     public static void main(String[] args) {
         for (int i = 0; i < 57; i++) {
             System.out.println("i**2=" + i * i);
+            System.out.println("abaaba");
         }
     }
 }
